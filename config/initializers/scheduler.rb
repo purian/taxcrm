05d955b17e0a26666bc08ec5537e3b91ecdb914a
purian <purian@gmail.com>
2024-05-24 19:58:37 +0300

    Create the services that scrape the data and update the scheduler to run them.

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -5,4 +5,8 @@ scheduler = Rufus::Scheduler.new
 # Schedule to run daily at midnight
 scheduler.cron '0 0 * * *' do
   FetchSalesDataService.new.call
+  FetchClientsDataService.new.call
+  FetchContactsDataService.new.call
+  FetchLeadsDataService.new.call
+  FetchReferralsDataService.new.call
 end
